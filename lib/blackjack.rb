1e427f1cae53690c302e0d1cd894db12ccf7bcf0
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-05 01:12:45 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,14 @@
 def welcome
-  # code #welcome here
+ puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  random(1..11)
+  
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
